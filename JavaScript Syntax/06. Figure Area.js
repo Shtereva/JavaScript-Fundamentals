982bcb509db139c8897281289df31e8bf46f9c6f
Shtereva <a.i.shtereva@gmail.com>
2018-09-23 16:27:26 +0300

    added gitignore

--- JavaScript Syntax/06. Figure Area.js
+++
@@ -1,8 +0,0 @@
-function figureArea(w, h, W, H){
-    let[s1, s2, s3] = [w * h, W * H, Math.min(h, H) * Math.min(w, W)];
-
-    let result = s1 + s2 - s3;
-    console.log(result);
-}
-
-figureArea(13, 2, 5, 8);
